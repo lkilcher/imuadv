faaade3336275288a66e50ef57e2ba9828d2e334
Levi Kilcher <Levi.Kilcher@nrel.gov>
2016-05-24 18:48:42 -0600

    I figured out the coherence between ubt and umot.
    The issues were:
    - motion correction of 'unfilt' gave a bad 'principal angle'
    - The accel data has to be filtered somewhat because otherwise the
      red noise in this signal overwhelms the coherence.

--- py/motion_coherene.py
+++ py/motion_coherene.py
@@ -18,7 +18,7 @@ flag['plot time'] = True
 filt_freqs = {'unfilt': 0.0,
               '5s': 1. / 5,
               # '10s': 1. / 10,
-              # '30s': 1. / 30
+              '30s': 1. / 30
 }
 
 pii = np.pi * 2
@@ -31,6 +31,9 @@ if 'bt' not in vars():
     bt = loadmat(data_api.package_root + 'ADCP/BT_IMU_sync_data.mat')
     bt['t_IMU'] = bt['t_IMU'][0] - 366
 
+bt_var = 'ubt'
+bt_var = 'ubt2'
+
 if 'dat' not in vars():
     # Load the 'raw' (unprocessed) data that corresponds to the file above.
     dat = data_api.load('SM_Nose', coordsys='raw')
@@ -40,13 +43,10 @@ if 'dat' not in vars():
     # Perform basic motion correction based on IMU data:
     bindat_filt = {}
     dat_filt = {}
-    for filt_tag, filt_freq in filt_freqs.iteritems():
+    for filt_tag in np.sort(filt_freqs.keys()):
+        filt_freq = filt_freqs[filt_tag]
         datmc = dat.copy()
         avm.motion.correct_motion(datmc, accel_filtfreq=filt_freq)
-
-        # The ADP data is in the instrument frame, so we make the ADV
-        # data consistent.
-        avm.rotate.inst2earth(datmc, reverse=True)
 
         # Crop the data so that it is consistent with the ADP data.
         datmc = datmc.subset(slice(offind, (offind + len(bt['t_IMU']))))
@@ -55,42 +55,44 @@ if 'dat' not in vars():
         motcalc = avm.motion.CalcMotion(datmc)
         ur_adp = motcalc.calc_urot(np.array([-1, 0, 0]), to_earth=False)
 
-        ubt = bt['BT_INST_interp']
-        ubt2 = bt['BT_INST_interp'] - ur_adp
+        datmc.add_data('ubt', bt['BT_INST_interp'], 'orient')
+        datmc.add_data('ubt2', bt['BT_INST_interp'] + ur_adp, 'orient')
+        bt_vars = {'ubt', 'ubt2'}
+        datmc.rotate_vars.update(bt_vars)
+        # The ADP data is in the instrument frame, so rotate it the earth frame.
+        avm.rotate.inst2earth(datmc, rotate_vars=bt_vars, force=True)
         if filt_freq > 0:
-            filt = sig.butter(2, filt_freq / (dat.fs / 2))
-            ubt = sig.filtfilt(filt[0], filt[1], ubt)
-            ubt2 = sig.filtfilt(filt[0], filt[1], ubt2)
-        datmc.add_data('ubt', ubt, 'orient')
-        datmc.add_data('ubt2', ubt2, 'orient')
+            filt = sig.butter(2, filt_freq / (datmc.fs / 2))
+            datmc.ubt = sig.filtfilt(filt[0], filt[1], datmc.ubt)
+            datmc.ubt2 = sig.filtfilt(filt[0], filt[1], datmc.ubt2)
         if filt_freq > 0:
-            datmc._u += datmc.ubt  # Add the bt to the vel
-        datmc.rotate_vars.update({'ubt', 'ubt2'})
-        avm.rotate.inst2earth(datmc)
+            datmc._u += datmc[bt_var]  # Add the bt to the vel
+        else:
+            # The principal angle for 'unfilt' is garbage:
+            datmc.props['principal_angle'] = dat_filt['5s'].props['principal_angle']
         avm.rotate.earth2principal(datmc)
         dat_filt[filt_tag] = datmc
 
         datnow = datmc
-
         datbd = binner(datnow)
-        datbd.Spec_uraw = binner.psd(datnow.uraw)
-        datbd.Spec_uacc = binner.psd(datnow.uacc)
-        datbd.Spec_urot = binner.psd(datnow.urot)
-        datbd.Spec_ubt = binner.psd(datnow.ubt)
+        datbd.add_data('Spec_uraw', binner.psd(datnow.uraw), 'Spec')
+        datbd.add_data('Spec_uacc', binner.psd(datnow.uacc), 'Spec')
+        datbd.add_data('Spec_urot', binner.psd(datnow.urot), 'Spec')
+        datbd.add_data('Spec_ubt', binner.psd(datnow.ubt), 'Spec')
         if filt_freq > 0:
-            datbd.Spec_umot = binner.psd(datnow.uacc +
-                                         datnow.urot +
-                                         datnow.ubt)
+            datbd.add_data('Spec_umot',
+                           binner.psd(datnow.uacc +
+                                      datnow.urot +
+                                      datnow[bt_var]),
+                           'Spec')
         else:
-            datbd.Spec_umot = binner.psd(datnow.uacc +
-                                         datnow.urot)
+            datbd.add_data('Spec_umot',
+                           binner.psd(datnow.uacc +
+                                      datnow.urot),
+                           'Spec')
         bindat_filt[filt_tag] = datbd
 
     dat = dat.subset(slice(offind, (offind + len(bt['t_IMU']))))
-
-# The mean velocity is totally contaminated by motion correction, so
-# lets fix it here
-bindat_filt['unfilt']['_u'] = bindat_filt['5s']['_u']
 
 
 def offset_scale(dat, offset, scale, ):
@@ -100,18 +102,25 @@ def offset_scale(dat, offset, scale, ):
 def within(dat, minval, maxval):
     return (minval < dat) & (dat < maxval)
 
-
-dnow = dat_filt['unfilt']
-bnow = bindat_filt['unfilt']
+dnow_name = 'unfilt'
+dnow_name = '30s'
+dnow = dat_filt[dnow_name]
 dnow.umot = dnow.urot + dnow.uacc
+if dnow_name != 'unfilt':
+    dnow.ubt = dat_filt['unfilt'].ubt
+    dnow.ubt2 = dat_filt['unfilt'].ubt2
+if dnow_name == 'unfilt':
+    inds = within(np.abs(bindat_filt['5s'].U), 1.0, 1.5)
+else:
+    inds = within(np.abs(bindat_filt[dnow_name].U), 1.0, 1.5)
 
-inds = within(np.abs(bnow.U), 1.0, 1.5)
 
 #n_fft = binner.n_bin / 4
 n_fft = binner.n_bin
 
 ubt = dnow.ubt
-ubt = dnow.ubt2
+#ubt = dnow.ubt2
+
 
 if flag.get('show cohere', False):
 
@@ -123,16 +132,17 @@ if flag.get('show cohere', False):
     sp_rot = np.abs(binner.cpsd(dnow.urot, dnow.urot, n_fft=n_fft))
 
     cpsda = binner.cpsd(dnow.uacc, ubt, n_fft=n_fft)
-    sp_acc = np.abs(binner.cpsd(dnow.uacc, dnow.uacc, n_fft=n_fft))
+    sp_acc = binner.cpsd(dnow.uacc, dnow.uacc, n_fft=n_fft)
+    cohfreq = binner.calc_omega() / pii
 
     # coh = ((np.abs(cpsd[:, inds]) ** 2).mean(1) /
     #        (sp_mot[:, inds].mean(1) * sp_bt[:, inds].mean(1)))
     coh = ((np.abs(cpsd[:, inds].mean(1)) ** 2) /
            (sp_mot[:, inds].mean(1) * sp_bt[:, inds].mean(1)))
-    coha = ((np.abs(cpsda[:, inds].mean(1)) ** 2) /
-            (sp_acc[:, inds].mean(1) * sp_bt[:, inds].mean(1)))
     cohr = ((np.abs(cpsdr[:, inds].mean(1)) ** 2) /
             (sp_rot[:, inds].mean(1) * sp_bt[:, inds].mean(1)))
+    coha = ((np.abs(cpsda[:, inds].mean(1)) ** 2) /
+            (sp_acc[:, inds].mean(1) * sp_bt[:, inds].mean(1)))
 
     fig = plt.figure(300, figsize=[6, 9])
     fig.clf()
@@ -145,11 +155,11 @@ if flag.get('show cohere', False):
                             sharex=True, sharey=True, )
 
     for iax, ax in enumerate(axs):
-        ax.semilogx(bnow.freq, coh[iax],
+        ax.semilogx(cohfreq, coh[iax],
                     'k', label='$u_{mot}$')
-        ax.semilogx(bnow.freq, cohr[iax],
+        ax.semilogx(cohfreq, cohr[iax],
                     'm', label='$u_{rot}$')
-        ax.semilogx(bnow.freq, coha[iax],
+        ax.semilogx(cohfreq, coha[iax],
                     'b', label='$u_{acc}$')
         ax.text(0.02, 0.98, ['u', 'v', 'w'][iax] + '-component',
                 transform=ax.transAxes,
@@ -165,6 +175,8 @@ if flag.get('show cohere', False):
 
 if flag.get('filt spec', False):
 
+    bnow = bindat_filt['5s'].copy()
+    # bnow.
     fig = plt.figure(400, figsize=[6, 9])
     fig.clf()
     fig, axs = plt.subplots(3, 1, num=fig.number,
@@ -206,29 +218,3 @@ if flag.get('filt spec', False):
                      dnow.props['coord_sys'], size='large')
 
     fig.savefig('../fig/NoMC_Spectra01.pdf')
-
-
-if flag.get('plot time', False):
-
-    tkr = dt.MinuteLocator(range(0, 60, 10))
-    #tkr = dt.HourLocator(range(0, 24, 1))
-    tk_fmt = dt.DateFormatter('%H:%M')
-    fig = plt.figure(100)
-    fig.clf()
-    fig, axs = plt.subplots(3, 1, num=fig.number, sharex=True, sharey=True)
-    #vars = ['Spec', 'Spec_uraw', 'Spec_uacc', 'Spec_urot', 'Spec_ubt', 'Spec_umot']
-
-    time_inds = slice(10000, 40000)
-
-    for iax, ax in enumerate(axs):
-        ax.plot(dnow.mpltime[time_inds], dnow._u[iax][time_inds], 'k-')
-        ax.plot(dnow.mpltime[time_inds], dnow.umot[iax][time_inds], 'b-')
-        ax.set_ylabel('${}'.format(['u', 'v', 'w'][iax] + '\ \mathrm{[m/s]}$'))
-
-    ax.xaxis.set_major_locator(tkr)
-    ax.xaxis.set_major_formatter(tk_fmt)
-
-    axs[0].set_title('Example velocity timeseries No MC (inst coord. sys.)')
-
-    fig.savefig('../fig/NoMC_TimeSeries01.pdf')
-    

ecab19b3837b54f06daa9ee7cbb0ea1defad8af9
Levi Kilcher <Levi.Kilcher@nrel.gov>
2017-02-06 07:57:20 -0700

    Update co-spectral plots
    - Add ogive plots
    - Add variance-preserving co-spectra.

--- py/stresses.py
+++ py/stresses.py
@@ -10,8 +10,9 @@ flag = {}
 # flag['amp/phase'] = True
 # flag['real/imag'] = True
 #flag['multi-real'] = True
-#flag['save fig'] = True
-flag['multi-ogive'] = True
+flag['multi-real-vp'] = True
+flag['save fig'] = True
+#flag['multi-ogive'] = True
 
 binners = dict(ttm=avm.TurbBinner(9600, 32),
                sm=avm.TurbBinner(4800, 16))
@@ -192,6 +193,70 @@ for idat, dat_nm in enumerate(['ttm', 'sm']):
 
         if flag.get('save fig'):
             fig.savefig(pt.figdir + 'StressSpec_{}_03.pdf'.format(dat_nm.upper()))
+
+    if flag.get('multi-real-vp'):
+
+        velranges = [(0, 0.5),
+                     (1, 1.5),
+                     (2, 2.5)]
+
+        fig, axs = pt.newfig(1000 * idat + 103,
+                             3, len(velranges),
+                             figsize=5,
+                             sharex=True, sharey=True,
+                             right=0.8, left=0.11,
+                             bottom=0.08)
+
+        f = bd.freq
+        for icol, vr in enumerate(velranges):
+            axcol = axs[:, icol]
+            inds = (vr[0] < bd['u']) & (bd['u'] < vr[1])
+            axs[0, icol].text(.9, .9, 'N={}'.format(inds.sum()),
+                              ha='right', va='top', fontsize='medium',
+                              transform=axs[0, icol].transAxes)
+            axs[0, icol].set_title(r"$%0.1f < \bar{u} < %0.1f$" % vr,
+                                   fontsize='medium')
+            for irow, ax in enumerate(axcol):
+                # r"$\overline{%s'%s'}=$%0.0e" % (pt.vel_comps[pairs[irow][0]],
+                #                                 pt.vel_comps[pairs[irow][1]],
+                #                                 np.trapz(dtmp, f), ),
+                dtmp = bd.Cspec_u[irow][inds].real * pii * f
+                # rng = np.empty((2, dtmp.shape[1]))
+                # for ifrq in range(dtmp.shape[1]):
+                #     rng[0, ifrq], mid, rng[1, ifrq] = pt.boot(dtmp[:, ifrq])
+                ax.semilogx(f, dtmp.mean(0), 'b-', label=pt.latex['ue'].cspec)
+                rng = (dtmp.std(0)[None, :] * np.array([[-1], [1]]) + dtmp.mean(0))
+                ax.fill_between(f, rng[0], rng[1], zorder=-12,
+                                facecolor=[0, 0, 1, 0.3], edgecolor='none')
+                # ax.semilogx(f, dtmp.T * pii, '-', color='0.7', zorder=-10)
+                ax.text(.9, .1,
+                        r"%2.2g" % (np.trapz(dtmp.mean(0), f) * 10000, ),
+                        ha='right', va='bottom',
+                        transform=ax.transAxes)
+                dtmp = bd.Cspec_umot[irow][inds].real * pii * f
+                ax.semilogx(f, dtmp.mean(0), 'r-', label=pt.latex['uhead'].cspec, zorder=-2)
+                dtmp = bd.Cspec_uraw[irow][inds].real * pii * f
+                ax.semilogx(f, dtmp.mean(0), 'k-', zorder=-5, label=pt.latex['umeas'].cspec)
+                ax.axhline(0, color='k', linestyle=':', zorder=-5, lw=1)
+
+        axs[0, -1].legend(loc='upper left', bbox_to_anchor=[1.1, 1],
+                          handletextpad=0.2, handlelength=2)
+
+        axs[0, 0].set_ylim([-0.005, 0.005])
+        # axs[0, 1].set_ylim([-np.pi, np.pi])
+        axs[0, 0].set_xlim(1e-3, 5)
+        for icol in range(axs.shape[1]):
+            axs[-1, icol].set_xlabel(r'$f\ \mathrm{[Hz]}$')
+        for irow in range(axs.shape[0]):
+            axs[irow, 0].set_ylabel('$[\mathrm{m^2s^{-2}}]$')
+            axs[irow, -1].text(1.03, 0.03,
+                               r'$C\{%s,%s\}$' % (pt.vel_comps[pairs[irow][0]],
+                                                  pt.vel_comps[pairs[irow][1]]),
+                               ha='left', va='bottom',
+                               transform=axs[irow, -1].transAxes)
+
+        if flag.get('save fig'):
+            fig.savefig(pt.figdir + 'StressSpec_{}_03vp.pdf'.format(dat_nm.upper()))
 
     if flag.get('multi-ogive'):
 

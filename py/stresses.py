e84e99ee4ed80eb663e723b3755b09ff23b7f597
Levi Kilcher <Levi.Kilcher@nrel.gov>
2017-02-23 21:03:42 -0700

    Single column cross-spec fig.

--- py/stresses.py
+++ py/stresses.py
@@ -4,6 +4,7 @@ import ptools as pt
 import dolfyn.adv.api as avm
 import numpy as np
 from scipy.integrate import cumtrapz
+import kaimal
 plt = pt.plt
 
 flag = {}
@@ -11,9 +12,10 @@ flag = {}
 # flag['real/imag'] = True
 #flag['multi-real'] = True
 #flag['multi-real-vp'] = True
-flag['uw-real'] = True
-#flag['save fig'] = True
+flag['multi-real-vp1'] = True
+#flag['uw-real'] = True
 #flag['multi-ogive'] = True
+flag['save fig'] = True
 
 binners = dict(ttm=avm.TurbBinner(9600, 32),
                sm=avm.TurbBinner(4800, 16))
@@ -44,7 +46,6 @@ if 'bdat' not in vars():
     bdat['sm'].z = 10
     bdat['ttt'].z = 4.6
 
-pt.twocol()
 pairs = [(0, 1), (0, 2), (1, 2)]
 
 do_data = [
@@ -57,6 +58,7 @@ for idat, dat_nm in enumerate(do_data):
     bd = bdat[dat_nm]
     if flag.get('amp/phase'):
 
+        pt.twocol()
         fig, axs = pt.newfig(1000 * idat + 101,
                              3, 2, figsize=5,
                              sharex=True, sharey='col',
@@ -115,6 +117,7 @@ for idat, dat_nm in enumerate(do_data):
 
     if flag.get('real/imag'):
 
+        pt.twocol()
         fig, axs = pt.newfig(1000 * idat + 102,
                              3, 2, figsize=5,
                              sharex=True, sharey=True,
@@ -164,6 +167,7 @@ for idat, dat_nm in enumerate(do_data):
                      (1, 1.5),
                      (2, 2.5)]
 
+        pt.twocol()
         fig, axs = pt.newfig(1000 * idat + 103,
                              3, len(velranges),
                              figsize=5,
@@ -228,7 +232,8 @@ for idat, dat_nm in enumerate(do_data):
                      (1, 1.5),
                      (2, 2.5)]
 
-        fig, axs = pt.newfig(1000 * idat + 103,
+        pt.twocol()
+        fig, axs = pt.newfig(1000 * idat + 113,
                              3, len(velranges),
                              figsize=5,
                              sharex=True, sharey=True,
@@ -239,8 +244,6 @@ for idat, dat_nm in enumerate(do_data):
         for icol, vr in enumerate(velranges):
             axcol = axs[:, icol]
             inds = (vr[0] < bd['u']) & (bd['u'] < vr[1])
-            # U = bd['u'][inds].mean() + 1j * bd['']
-            # kaimal = 
             axs[0, icol].text(.9, .9, 'N={}'.format(inds.sum()),
                               ha='right', va='top', fontsize='medium',
                               transform=axs[0, icol].transAxes)
@@ -274,10 +277,8 @@ for idat, dat_nm in enumerate(do_data):
 
             Uhor = np.abs(bd.U[inds]).mean()
             ustar2 = np.sqrt(bd.upwp_[inds]**2 + bd.vpwp_[inds]**2).mean()
-            fstar = f * z / Uhor
-            kaimal = 14 / (1 + 9.6 * fstar) ** (2.4)
-            kaimal *= -1 * z * ustar2 / Uhor
-            axs[1, icol].plot(f, kaimal, 'm-', lw=5)
+            kml = kaimal.Kaimal(f * bd.z / Uhor)
+            # axs[1, icol].plot(f, kml.Suw(), 'm-', lw=5)
 
         axs[0, -1].legend(loc='upper left', bbox_to_anchor=[1.05, 1],
                           handletextpad=0.2, handlelength=1.4)
@@ -309,6 +310,95 @@ for idat, dat_nm in enumerate(do_data):
         if flag.get('save fig'):
             fig.savefig(pt.figdir + 'StressSpec_{}_03vp.pdf'
                         .format(dat_nm.upper()))
+
+    if flag.get('multi-real-vp1'):
+
+        velranges = [(2, 2.5)]
+
+        #fctr = 1000
+        fctr = 100
+        with pt.style['onecol']():
+            fig, axs = pt.newfig(1000 * idat + 103,
+                                 3, len(velranges),
+                                 figsize=5.6,
+                                 squeeze=False, 
+                                 sharex=True, sharey=True,
+                                 right=0.7, left=0.15,
+                                 hspace=0.15,
+                                 bottom=0.08)
+
+            f = bd.freq
+            for icol, vr in enumerate(velranges):
+                axcol = axs[:, icol]
+                inds = (vr[0] < bd['u']) & (bd['u'] < vr[1])
+                # U = bd['u'][inds].mean() + 1j * bd['']
+                # kaimal = 
+                axs[0, icol].text(.9, .9, 'N={}'.format(inds.sum()),
+                                  ha='right', va='top', fontsize='medium',
+                                  transform=axs[0, icol].transAxes)
+                axs[0, icol].set_title(r"$%0.1f < \bar{u} < %0.1f$" % vr,
+                                       fontsize='medium')
+                for irow, ax in enumerate(axcol):
+                    # r"$\overline{%s'%s'}=$%0.0e" % (pt.vel_comps[pairs[irow][0]],
+                    #                                 pt.vel_comps[pairs[irow][1]],
+                    #                                 np.trapz(dtmp, f), ),
+                    dtmp = bd.Cspec_vel[irow][inds].real * pii * f * fctr
+                    # rng = np.empty((2, dtmp.shape[1]))
+                    # for ifrq in range(dtmp.shape[1]):
+                    #     rng[0, ifrq], mid, rng[1, ifrq] = pt.boot(dtmp[:, ifrq])
+                    ax.semilogx(f, dtmp.mean(0), 'b-',
+                                label=pt.latex['ue'].cspec_vp)
+                    rng = (dtmp.std(0)[None, :] * np.array([[-1], [1]]) + dtmp.mean(0))
+                    ax.fill_between(f, rng[0], rng[1], zorder=-12,
+                                    facecolor=[0, 0, 1, 0.3], edgecolor='none')
+                    # ax.semilogx(f, dtmp.T * pii, '-', color='0.7', zorder=-10)
+                    # ax.text(.94, .06,
+                    #         r"%2.1f" % (np.trapz(dtmp.mean(0) / f, f), ),
+                    #         ha='right', va='bottom',
+                    #         transform=ax.transAxes)
+                    dtmp = bd.Cspec_velmot[irow][inds].real * pii * f * fctr
+                    itmp = f < 0.0333
+                    ax.semilogx(f[itmp], dtmp.mean(0)[itmp], 'r--',
+                                zorder=-2)
+                    ax.semilogx(f[~itmp], dtmp.mean(0)[~itmp], 'r-',
+                                label=pt.latex['uhead'].cspec_vp, zorder=-2)
+                    dtmp = bd.Cspec_velraw[irow][inds].real * pii * f * fctr
+                    ax.semilogx(f, dtmp.mean(0), 'k-', zorder=-5,
+                                label=pt.latex['umeas'].cspec_vp)
+                    ax.axhline(0, color='k', linestyle=':', zorder=-5, lw=1)
+                    ax.axvline(0.03, color='r', ls=':', zorder=-10)
+
+            axs[0, -1].legend(loc='upper left', bbox_to_anchor=[1.05, 1],
+                              borderaxespad=0, prop=dict(size='medium'),
+                              handletextpad=0.2, handlelength=1.3)
+
+            axs[0, 0].set_ylim([-3, 3])
+            # axs[0, 1].set_ylim([-np.pi, np.pi])
+            axs[0, 0].set_xlim(1e-3, 5)
+            for icol in range(axs.shape[1]):
+                axs[-1, icol].set_xlabel(r'$f\ \mathrm{[Hz]}$')
+            for irow in range(axs.shape[0]):
+                # axs[irow, 0].set_ylabel('$f \cdot C\{%s,%s\} '
+                #                         '[10^{-3}\mathrm{m^2s^{-2}}]$' %
+                #                         (pt.vel_comps[pairs[irow][0]],
+                #                          pt.vel_comps[pairs[irow][1]]))
+                axs[irow, 0].set_ylabel('$[10^{-2}\,\mathrm{m^2s^{-2}}]$')
+                # axs[irow, -1].text(1.06, 0.03,
+                #                    r'$f \cdot C\{%s,%s\}$' % (
+                #                        pt.vel_comps[pairs[irow][0]],
+                #                        pt.vel_comps[pairs[irow][1]]),
+                #                    ha='left', va='bottom',
+                #                    transform=axs[irow, -1].transAxes)
+                axs[irow, -1].text(1.05, .1,
+                                   r'$f \cdot C\{%s,%s\}$' % (
+                                       pt.vel_comps[pairs[irow][0]],
+                                       pt.vel_comps[pairs[irow][1]]),
+                                   ha='left', va='bottom', size='large',
+                                   transform=axs[irow, 0].transAxes)
+
+            if flag.get('save fig'):
+                fig.savefig(pt.figdir + 'StressSpec_{}_04vp.pdf'
+                            .format(dat_nm.upper()))
 
     if flag.get('uw-real'):
 
@@ -389,6 +479,7 @@ for idat, dat_nm in enumerate(do_data):
         m73a = 0.1 * m73f ** (-7. / 3)
         df = 2e-2
         fstar_bins = np.arange(df, 10, df)
+        kml = kaimal.Kaimal(fstar_bins)
         #fstar_bins = np.logspace(-2, 1, 50)
         kaimal = 14.0 / (1 + 9.6 * fstar_bins) ** (2.4)
         Cdata, fstar = ndcs(bd, inds)
@@ -418,7 +509,7 @@ for idat, dat_nm in enumerate(do_data):
             fig, axs = pt.newfig(1000 * idat + 203,
                                  2, 1, **newfig_kws)
 
-            ax = axs[1]
+            ax = axs[0]
             # ax.loglog(fstar_bins, np.abs(Cs), 'b-', lw=1,
             #           label='Avg.')
             ax.loglog(fstar_bins, Cs, 'b-', lw=2,
@@ -431,30 +522,31 @@ for idat, dat_nm in enumerate(do_data):
                           label='TTT', zorder=-10)
             ax.loglog(m73f, m73a, 'r--', lw=2,
                       label='$\hat{f}^{-7/3}$')
-            ax.loglog(fstar_bins, kaimal, 'k-', lw=2, zorder=-2,
+            ax.loglog(fstar_bins, kml.Suw(), 'k-', lw=2, zorder=-2,
                       label='Kaimal')
             ax.set_xlim([1e-2, 10])
             ax.set_ylim([1e-4, 30])
             # z = 10
             ax.set_ylabel('$\hat{C}\{u,w\}$')
-            ax.set_xlabel('$\hat{f}$')
             ax.legend(loc='lower left',
                       prop=dict(size='small'), numpoints=1)
             # ax.set_title(ttl)
 
-            ax = axs[0]
+            ax = axs[1]
             ax.semilogx(fstar_bins, Cs2, 'b-', lw=2,
                         label='Avg.')
             ax.semilogx(fstar.flatten(),
                         fstar.flatten() * Cdata.flatten(),
                         '.', label='single', **pt_kws)
-            ax.semilogx(fstar_bins, fstar_bins * kaimal, 'k-',
+            ax.semilogx(fstar_bins, fstar_bins * kml.Suw(), 'k-',
                         label='Kaimal',
                         lw=2, zorder=-2)
             ax.set_xlim([1e-2, 10])
             ax.set_ylim([-1, 1])
             ax.set_ylabel('$\hat{f}\cdot \hat{C}\{u,w\}$')
-            ax.set_title(ttl)
+            
+            axs[-1].set_xlabel('$\hat{f}$')
+            axs[0].set_title(ttl)
 
             fig.savefig(pt.figdir + 'CoSpecND02_{}-{}.pdf'.format(
                 dat_nm.upper(), case),

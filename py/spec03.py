0a526f6f5fba9b4b0b4df76a73833f5e88bce0e7
Levi Kilcher <Levi.Kilcher@nrel.gov>
2017-02-24 17:38:45 -0700

    +Kaimal for torpedo

--- py/spec03.py
+++ py/spec03.py
@@ -3,6 +3,7 @@ import ptools as pt
 import numpy as np
 plt = pt.plt
 import dolfyn.adv.api as avm
+import kaimal
 
 doppler_noise = [1.5e-5, 1.5e-5, 0]
 
@@ -52,6 +53,13 @@ vard = dict(
     ),
 )
 
+z = 35
+U = 1.3
+f0 = U / z
+ustar2 = ((dat.upwp_**2 + dat.vpwp_**2).mean())**0.5
+
+kml = kaimal.Kaimal(dat.freq / f0)
+
 with pt.style['onecol']():
 
     velranges = [(0, 3),
@@ -99,6 +107,9 @@ with pt.style['onecol']():
                     ax.loglog(dat.freq[_itmp], dnow[_itmp], **kwd)
                 else:
                     ax.loglog(dat.freq, dnow, **kwd)
+            if irow in [0, 2]:
+                ax.loglog(dat.freq, kml[0] * ustar2 / f0, 'c-',
+                          zorder=10, label='Kaimal')
     for irow in range(axs.shape[0]):
         # The col-only loop
         axs[irow, 0].set_ylabel('$\mathrm{[m^2s^{-2}/Hz]}$')

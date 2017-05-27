8cb645acb09019a99d59b1164562353f82efadc0
Levi Kilcher <Levi.Kilcher@gmail.com>
2017-05-27 11:29:44 -0600

    Add db links to setup_data script

--- py/data/smb_may2015/setup_data.py
+++ py/data/smb_may2015/setup_data.py
@@ -5,19 +5,19 @@ import process_data as pd
 def pull():
     print("Retrieving SMB nose-mode data...")
     tbx.retrieve(
-        '<NEED LINK HERE!>... dropbox?',
+        'https://www.dropbox.com/s/wc2g87ynnygewif/StableMoor_Nose_NREL02_May2015.VEC?dl=1',
         datdir + 'SMN.VEC',
         hash='707ae7fa06c55d20'
     )
     print("Retrieving SMB Bottom-Track data...")
     tbx.retrieve(
-        '<NEED LINK HERE!>... dropbox?',
+        'https://www.dropbox.com/s/g7rrrj6457x0p5g/BT_IMU_sync_data.mat?dl=1',
         datdir + 'BT_IMU_sync_data.mat',
         hash='109bcde2ef771de9'
     )
     print("Retrieving Davit Torpedo data from May2015 (SMB deployment) ...")
     tbx.retrieve(
-        '<NEED LINK HERE!>... dropbox?',
+        'https://www.dropbox.com/s/snpy1a4xh4b8t1x/TTT_Davit_B_May2015.VEC?dl=1',
         datdir + 'TTT_Davit_B_May2015.VEC',
         hash='422831f6f5f1873f'
     )
